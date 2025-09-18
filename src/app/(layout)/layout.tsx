a3da818a1b4c8621417aa7b9b96e7a4461076c1d
Nodirbek12345 <natgress000@gmail.com>
2025-09-18 15:49:19 +0500

    first commit

--- src/app/(layout)/layout.tsx
+++ src/app/(layout)/layout.tsx
@@ -1,6 +1,5 @@
 import Footer from "@/shared/components/footer/Footer";
 import Header from "@/shared/components/header/Header";
-// import ScrollUp from "@/shared/components/scroll-up/scroll-up";
 import { ReactNode } from "react";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
